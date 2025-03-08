373893c875a841a8de2931b3625be696a9dcb7a7
Maximilien <40921999+Maxodie@users.noreply.github.com>
2025-03-08 12:06:30 +0100

    feat : vimgrep shortcut on <leader>ps

--- lazy/telescope.lua
+++ lazy/telescope.lua
@@ -21,9 +21,20 @@ return {
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end, {desc = "string grep upper"})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end, {desc = "grep mais des fois seulement"})
+
+        vim.keymap.set('n', '<leader>psa', ":vimgrep //g **/*<left><left><left><left><left><left><left>", { desc = "vimgrep in all project"})
+        vim.keymap.set('n', '<leader>psf', ":vimgrep //g %<left><left><left><left>", { desc = "vimgrep in current file"})
+        vim.keymap.set('n', '<leader>pso', ":cope", { desc = "vimgrep open result (:cope)"})
+
+        vim.keymap.set('n', '<leader>psn', ":cnext", { desc = "vimgrep next result (:cnext)"})
+        vim.keymap.set('n', '<leader>psp', ":cprev", { desc = "vimgrep previous result (:cprev)"})
+
+        vim.keymap.set('n', '<leader>psu', ":cprev", { desc = "vimgrep first result (:cfirst)"})
+        vim.keymap.set('n', '<leader>psd', ":clast", { desc = "vimgrep last result (:clast)"})
+
+        --vim.keymap.set('n', '<leader>ps', function()
+            --builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        --end, {desc = "grep mais des fois seulement"})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {}, {desc = "telescope help tags"})
     end
 }
